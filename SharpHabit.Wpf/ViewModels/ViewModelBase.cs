fc9932fdce3a5a0a47032cb1d4fa2e1f82cebf5b
Ivan Ganchev <djonatan2002@gmail.com>
2025-12-17 19:14:23 +0200

    Create ViewModelBase

---
+++ SharpHabit.Wpf/ViewModels/ViewModelBase.cs
@@ -0,0 +1,9 @@
+﻿using System.ComponentModel;
+
+namespace SharpHabit.Wpf.ViewModels
+{
+    public class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+    }
+}
