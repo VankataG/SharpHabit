4945f7b3511e325c485cf6a9f7513add68090666
Ivan Ganchev <djonatan2002@gmail.com>
2025-12-19 06:56:45 +0200

    Create MainVM for all habits

---
+++ SharpHabit.Wpf/ViewModels/MainViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.ObjectModel;
+
+namespace SharpHabit.Wpf.ViewModels
+{
+    public class MainViewModel : ViewModelBase
+    {
+        public ObservableCollection<HabitItemViewModel> Habits { get; } = new();
+
+        public MainViewModel()
+        {
+            Habits.Add(new HabitItemViewModel { Name = "Wokout" });
+            Habits.Add(new HabitItemViewModel { Name = "Code" });
+            Habits.Add(new HabitItemViewModel { Name = "Creatine" });
+        }
+    }
+}
