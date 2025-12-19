3f5fab62fab7f2b8854c9b00f74d66f72f435493
Ivan Ganchev <djonatan2002@gmail.com>
2025-12-19 18:57:41 +0200

    Insert AddHabitCommand in MainVM

--- SharpHabit.Wpf/ViewModels/MainViewModel.cs
+++ SharpHabit.Wpf/ViewModels/MainViewModel.cs
@@ -1,4 +1,6 @@
 ﻿using System.Collections.ObjectModel;
+using System.Windows.Input;
+using SharpHabit.Wpf.Commands;
 
 namespace SharpHabit.Wpf.ViewModels
 {
@@ -6,11 +8,23 @@ namespace SharpHabit.Wpf.ViewModels
     {
         public ObservableCollection<HabitItemViewModel> Habits { get; } = new();
 
+        public ICommand AddHabitCommand { get; }
+
         public MainViewModel()
         {
             Habits.Add(new HabitItemViewModel { Name = "Wokout" });
             Habits.Add(new HabitItemViewModel { Name = "Code" });
             Habits.Add(new HabitItemViewModel { Name = "Creatine" });
+
+            AddHabitCommand = new RelayCommand(AddHabit, () => Habits.Count < 5);
+        }
+
+        private void AddHabit()
+        {
+            Habits.Add(new HabitItemViewModel
+            {
+                Name = "New habit",
+            });
         }
     }
 }
