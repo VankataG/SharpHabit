53dfca73aeb293f475d31ee9980966b1e2e6330e
Ivan Ganchev <djonatan2002@gmail.com>
2025-12-19 06:56:13 +0200

    Create Habit vm

---
+++ SharpHabit.Wpf/ViewModels/HabitItemViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace SharpHabit.Wpf.ViewModels
+{
+    public class HabitItemViewModel : ViewModelBase
+    {
+        private string name = "";
+        private bool isDoneToday;
+
+        public string Name { get => name; set => SetField(ref name, value);}
+
+        public bool IsDoneToday { get => isDoneToday; set => SetField(ref isDoneToday, value);}
+    }
+}
