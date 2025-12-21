b8b79242927cc77430e154ac9a367aa2ec1caffc
Ivan Ganchev <djonatan2002@gmail.com>
2025-12-21 12:03:08 +0200

    Add Playground project for testing while creating

---
+++ SharpHabit.Playground/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Xml;
+using SharpHabit.Core;
+using SharpHabit.Core.Models;
+
+namespace SharpHabit.Playground
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, Ivan!");
+
+            HabitTracker tracker = new HabitTracker();
+
+            Habit study = tracker.AddHabit("Study");
+            Habit workout = tracker.AddHabit("Workout");
+
+            DateOnly today = DateOnly.FromDateTime(DateTime.Now);
+
+            var toggled = tracker.Toggle(study.Id, today);
+            tracker.Toggle(workout.Id, today);
+
+            Console.WriteLine(toggled);
+
+            Console.WriteLine($"Done today: {tracker.DayPercentage(today)}%");
+            Console.WriteLine($"Month: {tracker.MonthPercentage(today.Year, today.Month):f2}%");
+        }
+    }
+}
