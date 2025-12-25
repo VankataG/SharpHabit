b3a2f798b3e6676a2d1773bda895c816ae38dea1
Ivan Ganchev <djonatan2002@gmail.com>
2025-12-25 10:45:08 +0200

    Add ObservableObejct for each cell in the grid row

---
+++ SharpHabit.Wpf/ViewModels/HabitDayCellViewModel.cs
@@ -0,0 +1,20 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace SharpHabit.Wpf.ViewModels
+{
+    public partial class HabitDayCellViewModel : ObservableObject
+    {
+        public Guid HabitId { get; }
+
+        public DateOnly Date { get; }
+
+        [ObservableProperty]
+        private bool isDone;
+
+        public HabitDayCellViewModel(Guid habitId, DateOnly date)
+        {
+            this.HabitId = habitId;
+            this.Date = date;
+        }
+    }
+}
