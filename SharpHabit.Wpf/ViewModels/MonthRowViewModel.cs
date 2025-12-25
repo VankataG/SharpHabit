8ffc97d8abf685cf20e24dee16e2165fc5b801e2
Ivan Ganchev <djonatan2002@gmail.com>
2025-12-25 10:45:33 +0200

    Add ObservableObject for row in the grid

---
+++ SharpHabit.Wpf/ViewModels/MonthRowViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.ObjectModel;
+using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace SharpHabit.Wpf.ViewModels
+{
+    public class MonthRowViewModel : ObservableObject
+    {
+        public Guid HabitId { get; }
+
+        public string HabitName { get; } = null!;
+
+        public ObservableCollection<HabitDayCellViewModel> Cells { get; } = new();
+
+        public MonthRowViewModel(Guid habitId, string habitName, int year, int month, int daysInMonth)
+        {
+            this.HabitId = habitId;
+            this.HabitName = habitName;
+        }
+    }
+}
