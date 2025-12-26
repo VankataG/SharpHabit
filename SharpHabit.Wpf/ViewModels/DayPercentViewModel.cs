15046167d4743d6a121ec83c9e25f16b135809f9
Ivan Ganchev <djonatan2002@gmail.com>
2025-12-26 08:52:23 +0200

    Create VM for the percentage chart

---
+++ SharpHabit.Wpf/ViewModels/DayPercentViewModel.cs
@@ -0,0 +1,17 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace SharpHabit.Wpf.ViewModels
+{
+    public partial class DayPercentViewModel : ObservableObject
+    {
+        public int DayNumber { get; }
+
+        [ObservableProperty]
+        private double percent;
+
+        public DayPercentViewModel(int dayNumber)
+        {
+            this.DayNumber = dayNumber;
+        }
+    }
+}
