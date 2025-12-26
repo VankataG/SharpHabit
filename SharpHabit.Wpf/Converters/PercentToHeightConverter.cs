9632b8fdfbcaae0b4222e4066d530ffe4e2f2958
Ivan Ganchev <djonatan2002@gmail.com>
2025-12-26 15:28:13 +0200

    Create IValueConverter for the percent chart

---
+++ SharpHabit.Wpf/Converters/PercentToHeightConverter.cs
@@ -0,0 +1,23 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+
+namespace SharpHabit.Wpf.Converters
+{
+    public class PercentToHeightConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is double percent)
+            {
+                return percent * 1.2;
+            }
+
+            return 0.0;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
