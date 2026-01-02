4774ca7a94c96c1967fb9b5c9c345c572507af34
Ivan Ganchev <djonatan2002@gmail.com>
2026-01-02 11:09:44 +0200

    Add static FileStoragePath

---
+++ SharpHabit.Infrastructure/AppPaths.cs
@@ -0,0 +1,16 @@
+﻿namespace SharpHabit.Infrastructure
+{
+    public static class AppPaths
+    {
+        public static string StorageFilePath 
+        {
+            get
+            {
+                string appData = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+                string appFolder = Path.Combine(appData, "SharpHabit");
+
+                return Path.Combine(appFolder, "sharphabit.json");
+            }
+        }
+    }
+}
